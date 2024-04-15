2fdc9be21fad6b7dcf3bcf384d37780284c0950b
IkoMaster <qafarovilkin2003@gmail.com>
2024-04-15 23:06:58 +0400

    Enriched UI
    Added GameOver screen and button to restart the game.

--- Assets/RootBib.cs
+++ Assets/RootBib.cs
@@ -1,12 +1,16 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class RootBib : MonoBehaviour
 {
     public Rigidbody2D bibBody;
     public byte flyStrength;
+    private bool isBibAlive = true;
+    [SerializeField] private GameObject gameover;
 
     public delegate void OnPassedPipeEnter(int Score);
     public event OnPassedPipeEnter OnPassedPipe;
@@ -21,7 +25,7 @@ public class RootBib : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) == true) {
+        if (Input.GetKeyDown(KeyCode.Mouse0) && isBibAlive) {
            bibBody.velocity = Vector2.up * flyStrength;
         }
 
@@ -33,8 +37,41 @@ public class RootBib : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        if (collision.gameObject.name == "scoreup")
+        {
+            ScoreUp();
+        }
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "toppipe" || collision.gameObject.name == "bottompipe")
+        {
+            GameOver();
+        }
+    }
+
+
+    private void ScoreUp()
+    {
         Score++;
         OnPassedPipe?.Invoke(Score);
     }
+    private void GameOver()
+    {
+        gameover.SetActive(true);
+        NotAlive();
+    }
 
+    private void NotAlive()
+    {
+        isBibAlive = false;
+    }
+
+    [ContextMenu("restart")]public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("works");
+    }
 }
